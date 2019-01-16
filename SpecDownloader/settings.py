6db3fb42d6db844b78537664c9e3724e87dc8861
chenhechun <chenhechun@hangsheng.com.cn>
2019-01-16 14:22:26 +0800

    Modify the timeout value, so it can download the large file now.

--- SpecDownloader/settings.py
+++ SpecDownloader/settings.py
@@ -28,6 +28,8 @@ ROBOTSTXT_OBEY = True
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
+DOWNLOAD_TIMEOUT = 1800
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
